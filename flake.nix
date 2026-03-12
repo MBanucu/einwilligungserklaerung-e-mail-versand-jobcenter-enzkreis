a9545f80dd56b7a1686719a4d1d1d5064edfb417
MBanucu <michael.banucu@googlemail.com>
2026-03-12 16:09:17 +0100

    chore: update nixpkgs input to nixpkgs-unstable and update lock

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "basic texlive dev shell with tectonic and texlab";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
